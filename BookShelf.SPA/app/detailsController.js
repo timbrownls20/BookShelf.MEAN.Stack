55dc240c4bdb67bd7d605790b8fe6591d0c1ab77
timbrownls20 <timbrownls26@hotmail.com>
2016-12-03 16:16:02 +0000

    Initial commit

---
+++ BookShelf.SPA/app/detailsController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('detailsController', detailsController);
+
+    function detailsController($scope, $routeParams, bookService) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'detailsController';
+        var id = $routeParams.id;
+        
+        bookService.get(id)
+            .then(function (response) {
+                $scope.book = response.data;
+            });
+
+    }
+})();
