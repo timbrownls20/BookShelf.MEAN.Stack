e09aa3fc89f44ceb9d2262737c62d5245d79d7e7
timbrownls20 <timbrownls26@hotmail.com>
2017-01-27 17:56:46 +0000

    Extend book schema

--- BookShelf.API/app_api/models/books.js
+++ BookShelf.API/app_api/models/books.js
@@ -1,5 +1,12 @@
 var mongoose = require('mongoose');
 
+var authorSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+});
+
 var bookSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -7,8 +14,22 @@ var bookSchema = new mongoose.Schema({
     },
     description: {
         type: String,
-        required: true
+        required: false
     },
+    publisher: {
+        type: String,
+        required: false
+    },
+    pageCount: {
+        type: Number,
+        required: false
+    },
+    publishedDate: {
+        type: Date,
+        required: false
+    },
+    authors: [authorSchema]
+
 });
 
 mongoose.model('Book', bookSchema);
